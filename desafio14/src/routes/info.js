77f089379b8a63588407ed6c401ff7d2f865ebcd
Pablo Groba <pgroba@gmail.com>
2022-07-02 19:58:11 -0300

    desafio14

---
+++ desafio14/src/routes/info.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import os from 'os';
+import compression from 'compression';
+import logger from '../log4js/logger.js';
+
+export const info = Router();
+
+info.get('/', compression(), (req, res) => {
+  logger.info(`Ruta: ${req.url} - Metodo: ${req.method}`);
+  console.log('Paso por aca');
+  res.send({
+    argumentos_entrada: process.argv.slice(2),
+    SO: process.platform,
+    version_node: process.version,
+    RSS: process.memoryUsage().rss,
+    PATH_de_ejecucion: process.execPath,
+    PROCESS_ID: process.pid,
+    Carpeta_del_proyecto: process.cwd(),
+    numero_de_procesadores: os.cpus().length,
+  });
+});
