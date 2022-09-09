e6e1a381c495ef859032412203f6753a086ba64d
Pablo Groba <pgroba@gmail.com>
2022-09-09 09:09:20 -0300

    final

--- final/controllers/pedidoController.js
+++ final/controllers/pedidoController.js
@@ -2,6 +2,11 @@ import PedidosRepo from '../persistencia/repositorio/PedidosRepo.js';
 import CarritosRepo from '../persistencia/repositorio/CarritosRepo.js';
 import Pedido from '../negocio/Pedido.js';
 import Carrito from '../negocio/Carrito.js';
+import { transporter } from '../nodemailer/config.js';
+import {
+	adminEmailTemplate,
+	userEmailTemplate,
+} from '../nodemailer/templates.js';
 import { v4 as uuidv4 } from 'uuid';
 
 const pedidosRepo = new PedidosRepo();
@@ -29,5 +34,21 @@ export const saveOrder = async (req, res) => {
 	});
 	await pedidosRepo.guardar(pedido);
 	await carritosRepo.actualizar(new Carrito({ id: req.user.id, prods: [] }));
+	/* email */
+
+	const adminMail = adminEmailTemplate(
+		process.env.ADMIN_MAIL,
+		pedido.datos(),
+		req.user.email
+	);
+	const userMail = userEmailTemplate(req.user.email, pedido.datos());
+
+	try {
+		await transporter.sendMail(adminMail);
+		await transporter.sendMail(userMail);
+	} catch (error) {
+		console.log(error);
+	}
+
 	res.json(await pedidosRepo.listar(pedido.id));
 };
